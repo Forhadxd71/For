dbc7b1506e356add6c7d6e85b48a42557c486895
Forhad71 <112680572+Forhadxd71@users.noreply.github.com>
2024-07-09 06:19:20 +0600

    Update main.py

--- main.py
+++ main.py
@@ -24,20 +24,21 @@ dp = Dispatcher(bot)
 client = TelegramClient(StringSession(), api_id, api_hash)
 
 async def forward_media(message: types.Message, forwarded_channels):
+    media = []
     if message.photo:
-        media = InputMediaPhoto(media=message.photo[-1].file_id)
+        media = [InputMediaPhoto(media=photo.file_id) for photo in message.photo]
     elif message.video:
-        media = InputMediaVideo(media=message.video.file_id)
+        media = [InputMediaVideo(media=message.video.file_id)]
     else:
         return
 
     for channel in forwarded_channels:
-        await bot.send_media_group(chat_id=channel, media=[media])
+        await bot.send_media_group(chat_id=channel, media=media)
 
 @dp.message_handler(content_types=[types.ContentType.PHOTO, types.ContentType.VIDEO])
 async def handle_new_media(message: types.Message):
     chat_id = message.chat.id
-    if chat_id in collected_channels:
+    if str(chat_id) in collected_channels:
         await forward_media(message, forwarded_channels)
 
 async def startup(dispatcher):
@@ -50,15 +51,18 @@ async def startup(dispatcher):
             if message.photo or message.video:
                 # Create a mock aiogram Message object
                 wrapped_message = types.Message(
-                    message.id,
-                    from_user=types.User(id=message.from_id.user_id),
+                    message_id=message.id,
+                    from_user=types.User(id=message.sender_id),
                     date=message.date.timestamp(),
                     chat=types.Chat(id=message.chat_id),
                     content_type='photo' if message.photo else 'video',
-                    bot=dispatcher.bot,
+                    bot=dispatcher.bot
                 )
-                wrapped_message.photo = message.photo
-                wrapped_message.video = message.video
+                if message.photo:
+                    wrapped_message.photo = [types.PhotoSize(file_id=photo.id) for photo in message.photo]
+                if message.video:
+                    wrapped_message.video = types.Video(file_id=message.video.id)
+                
                 await forward_media(wrapped_message, forwarded_channels)
 
     # Start polling
