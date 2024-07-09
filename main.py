ed0d1cd996de090fb6a9fb3e794cf1f085c30946
Forhad71 <112680572+Forhadxd71@users.noreply.github.com>
2024-07-09 07:07:35 +0600

    Update main.py

--- main.py
+++ main.py
@@ -2,10 +2,10 @@ import io
 from telethon import TelegramClient, sync
 from telethon.tl.types import InputMessagesFilterPhotos, InputMessagesFilterVideo, InputMessagesFilterGif
 
-api_id = 9307366
-api_hash = '1ce7a3a4670658d10a01b7e6b090fc07'
-source_channels = ['unofficialmagi']  # Source channels to collect media from
-destination_channels = ['gsjjsbsbsb']  # Destination channels to forward media to
+api_id = 24232038
+api_hash = '6b55079d2ba17ccc133881d67df066a9'
+source_channels = ['unofficialmagi', '+fDZo8WCjA-FhNDMy', 'RedZoneDrop', 'nudobazz', 'publicnudity11']  # Add more source channels here
+destination_channels = ['+c4uZQoRyVnQ3MWM9']  # Destination channels to forward media to
 pic_down = './pic'
 gif_down = './gif'
 video_down = './video'
@@ -32,57 +32,30 @@ def forward_message(client, message, destinations):
     for destination in destinations:
         client.forward_messages(destination, message)
 
-def getPhotoList(client, channel):
+def getMediaList(client, channel, filter_type, download_path, file_extension):
     channel_link = 'https://t.me/' + channel
-    photos = client.get_messages(channel_link, None, filter=InputMessagesFilterPhotos)
-    total = len(photos)
+    messages = client.get_messages(channel_link, None, filter=filter_type)
+    total = len(messages)
     index = 0
-    for photo in photos:
-        filename = pic_down + "/" + channel + "/" + str(photo.id) + ".jpg"
+    for message in messages:
+        filename = f"{download_path}/{channel}/{message.id}.{file_extension}"
         index += 1
         if IsChongFu(filename, history_file):
             continue
-        print("downloading: ", index, "/", total, " : ", filename)
-        download(client, photo, filename)
-        forward_message(client, photo, destination_channels)
-    print('photos are done..')
+        print(f"downloading: {index}/{total} : {filename}")
+        download(client, message, filename)
+        forward_message(client, message, destination_channels)
+    print(f'{filter_type.__name__.split("InputMessagesFilter")[1].lower()}s are done..')
 
-def getGifList(client, channel):
-    channel_link = 'https://t.me/' + channel
-    gifs = client.get_messages(channel_link, None, filter=InputMessagesFilterGif)
-    total = len(gifs)
-    index = 0
-    for gif in gifs:
-        filename = gif_down + "/" + channel + "/" + str(gif.id) + ".mp4"
-        index += 1
-        if IsChongFu(filename, history_file):
-            continue
-        print("downloading: ", index, "/", total, " : ", filename)
-        download(client, gif, filename)
-        forward_message(client, gif, destination_channels)
-    print('gifs are done..')
-
-def getVideoList(client, channel):
-    channel_link = 'https://t.me/' + channel
-    videos = client.get_messages(channel_link, None, filter=InputMessagesFilterVideo)
-    total = len(videos)
-    index = 0
-    for video in videos:
-        filename = video_down + "/" + channel + "/" + str(video.id) + ".mp4"
-        index += 1
-        if IsChongFu(filename, history_file):
-            continue
-        print("downloading: ", index, "/", total, " : ", filename)
-        download(client, video, filename)
-        forward_message(client, video, destination_channels)
-    print('videos are done..')
+def process_channel(client, channel):
+    print(channel + ' is starting...')
+    getMediaList(client, channel, InputMessagesFilterPhotos, pic_down, "jpg")
+    getMediaList(client, channel, InputMessagesFilterGif, gif_down, "mp4")
+    getMediaList(client, channel, InputMessagesFilterVideo, video_down, "mp4")
 
 if __name__ == "__main__":
     client = TelegramClient('my_session', api_id=api_id, api_hash=api_hash).start()
     for channel in source_channels:
-        print(channel + ' is starting...')
-        getPhotoList(client, channel)
-        getGifList(client, channel)
-        getVideoList(client, channel)
+        process_channel(client, channel)
     client.disconnect()
     print('ALL done !!')
